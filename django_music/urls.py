7a13c5c7738476e458664043eaed826806a78f97
andres alcocer <devandres.tech@gmail.com>
2022-06-30 15:02:36 -0400

    now able to edit albums

--- django_music/urls.py
+++ django_music/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', albums_views.list_albums, name='list_albums'),
     path('albums/new', albums_views.new_album, name='new_album'),
     path('albums/<int:pk>', albums_views.detail_album, name='detail_album'),
+    path('albums/<int:pk>/edit', albums_views.edit_album, name='edit_album'),
 ]
