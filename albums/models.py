762b2d1051a84bad94cebc6766613d9284d253ba
andres alcocer <devandres.tech@gmail.com>
2022-06-30 14:21:05 -0400

    now able to add new albums

--- albums/models.py
+++ albums/models.py
@@ -6,9 +6,15 @@ class Artist(models.Model):
 	name = models.CharField(max_length=255)
 	birthdate = models.DateField(null=True, blank=True)
 
+	def __str__(self):
+		return self.name
+
 
 class Album(models.Model):
 	title = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add=True)
 	artist = models.ManyToManyField(Artist)
+
+	def __str__(self):
+		return self.title
 
--- albums/views.py
+++ albums/views.py
@@ -1,6 +1,7 @@
 from asyncio import constants
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Artist, Album
+from .forms import AlbumForm
 
 
 # Create your views here.
@@ -8,3 +9,13 @@ def list_albums(request):
 	albums = Album.objects.all()
 	return render(request, 'albums/list_albums.html', {'albums': albums})
 
+
+def new_album(request):
+	if request.method == 'GET':
+		form = AlbumForm()
+	else:
+		form = AlbumForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect(to='list_albums')
+	return render(request, 'albums/new_album.html', {'form': form})
