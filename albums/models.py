7443f057addb4a0347fadb61dad46a8472fca152
andres alcocer <devandres.tech@gmail.com>
2022-06-30 13:10:43 -0400

    able to render album list in home page

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Artist(models.Model):
+	name = models.CharField(max_length=255)
+	birthdate = models.DateField(null=True, blank=True)
+
+
+class Album(models.Model):
+	title = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	artist = models.ManyToManyField(Artist)
+
--- django_music/urls.py
+++ django_music/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from albums import views as albums_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', albums_views.list_albums, name='list_albums')
 ]
